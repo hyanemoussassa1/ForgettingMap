2a29b812b16565add4e25b4a1872405e85940878
Hyane Moussassa <hyane.moussassa1@gmail.com>
2022-01-24 14:31:29 +0000

    Added tests and features for forgetting map, include add and find

--- src/main/java/com/origamienergy/forgettingmap/ForgettingMap.java
+++ src/main/java/com/origamienergy/forgettingmap/ForgettingMap.java
@@ -1,4 +1,77 @@
 package com.origamienergy.forgettingmap;
 
-public class ForgettingMap {
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+public class ForgettingMap<K, V>{
+
+    private final Map<K, V> entries;
+    private final ConcurrentHashMap<K, Integer> accessCounts;
+
+    private final int MAX_ENTRIES;
+
+    public ForgettingMap(int maxEntries) {
+        this.MAX_ENTRIES = maxEntries;
+        entries = Collections.synchronizedMap(new LinkedHashMap<>(maxEntries, .75f, true));
+        accessCounts = new ConcurrentHashMap<>(MAX_ENTRIES);
+    }
+
+    public boolean add(K key, V content) {
+        synchronized (entries) {
+            entries.put(key, content);
+            while (this.entries.size() > MAX_ENTRIES)
+                removeTopmostUnused();
+
+            if (!accessCounts.containsKey(key))
+                accessCounts.put(key, 0);
+        }
+        return true;
+    }
+
+    private void removeTopmostUnused() {
+        Set<K> leastUsedKeys = getLeastUsedKeys();
+        for (K key: leastUsedKeys){
+            if (this.entries.containsKey(key))
+                this.entries.remove(key);
+
+            if (this.accessCounts.containsKey(key))
+                this.accessCounts.remove(key);
+        }
+    }
+
+
+    private Set<K> getLeastUsedKeys() {
+        int minAccessValue = accessCounts.values().stream().min(Integer::compareTo).get();
+        Set<K> minValuesSet = accessCounts.entrySet()
+                .stream()
+                .filter((Map.Entry<K, Integer> entry) -> entry.getValue() == minAccessValue)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+
+        for (K key : entries.keySet()) {
+            if (minValuesSet.contains(key)) {
+                return Set.of(key);
+            }
+        }
+        return minValuesSet;
+    }
+
+    public V find(K key) {
+        int oldValue = 0;
+        if (accessCounts.containsKey(key))
+            oldValue = accessCounts.get(key);
+
+        accessCounts.put(key, ++oldValue);
+        return entries.get(key);
+    }
+
+    public int size(){
+        return entries.size();
+    }
+
+    public Set<K> keySet() {
+        return entries.keySet();
+    }
 }
--- src/test/java/com/origamienergy/forgettingmap/ForgettingMapTests.java
+++ src/test/java/com/origamienergy/forgettingmap/ForgettingMapTests.java
@@ -1,15 +1,185 @@
 package com.origamienergy.forgettingmap;
 
+import net.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Set;
+import java.util.concurrent.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ForgettingMapTests {
 
     @Test
-    public void testThatApplicationRuns(){
-        ForgettingMap map = new ForgettingMap();
-        assertNotNull(map);
+    public void testForgettingMapCanAddAnAssociation(){
+        ForgettingMap map = new ForgettingMap(10);
+        boolean success = map.add("key", "content");
+        assertTrue(success);
+    }
+
+    @Test
+    public void testForgettingMapCanFindAnExistingAssociation(){
+        ForgettingMap map = new ForgettingMap(10);
+        String key = "key";
+        String content = "content";
+        map.add(key, content);
+        String contentFound = (String) map.find(key);
+        assertEquals(contentFound, content);
+    }
+
+    @Test
+    public void testForgettingMapCanAddAnAssociationOfIntegerKey(){
+        ForgettingMap map = new ForgettingMap(10);
+        boolean success = map.add(10, "content");
+        assertTrue(success);
+    }
+
+    @Test
+    public void testForgettingMapAddsNoMoreThanXAssociations(){
+        ForgettingMap map = new ForgettingMap(3);
+        map.add(10, "content_10");
+        map.add(15, "content_15");
+        map.add(20, "content_20");
+        map.add(50, "content_30");
+        assertEquals(3, map.size());
+    }
+
+    @Test
+    public void testForgettingMapDoesNotContainTopmostUnusedAssociation(){
+        ForgettingMap map = new ForgettingMap(3);
+        map.add(10, "content_10");
+        map.add(15, "content_15");
+        map.add(20, "content_20");
+        map.add(50, "content_50");
+        assertNotNull(map.find(15));
+        assertNotNull(map.find(20));
+        assertNotNull(map.find(50));
+        String notFound = (String) map.find(10);
+        assertNull(notFound);
+    }
+
+    @Test
+    public void testForgettingMapReturnsAccessOrder(){
+        ForgettingMap map = new ForgettingMap(3);
+        map.add(0, "content_0");
+        map.add(15, "content_15");
+        map.add(30, "content_30");
+        Set<Integer> keys = map.keySet();
+        assertEquals("[0, 15, 30]", keys.toString());
+        map.find(15);
+        assertEquals("[0, 30, 15]", keys.toString());
+        map.find(0);
+        assertEquals("[30, 15, 0]", keys.toString());
+    }
+
+
+    @Test
+    public void testForgettingMapDoesNotContainLeastRetrievedByFind(){
+        ForgettingMap map = new ForgettingMap(3);
+        map.add(0, "content_0");
+        map.add(15, "content_15");
+        map.add(30, "content_30");
+        Set<Integer> keys = map.keySet();
+        map.find(15);
+        map.find(15);
+        map.find(15);
+        map.find(0);
+        map.find(30);
+        map.find(30);
+        map.add(40, "content_40");
+        assertEquals(3, map.size());
+        assertTrue(keys.contains(15));
+        assertTrue(keys.contains(30));
+        assertTrue(keys.contains(40));
+        assertFalse(keys.contains(0));
+    }
+
+    @Test
+    public void testForgettingMapDoesNotContainEntriesNotFound(){
+        ForgettingMap map = new ForgettingMap(3);
+        map.add(30, "content_0");
+        map.add(15, "content_15");
+        map.add(0, "content_30");
+        Set<Integer> keys = map.keySet();
+        map.find(15);
+        map.find(0);
+        map.add(40, "content_40");
+        assertTrue(keys.contains(15));
+        assertTrue(keys.contains(0));
+        assertTrue(keys.contains(40));
+        assertFalse(keys.contains(30));
+    }
+
+    @Test
+    public void testForgettingMapWithMultipleLeastUsedEntries(){
+        ForgettingMap map = new ForgettingMap(3);
+        map.add(0, "content_0");
+        map.add(15, "content_15");
+        map.add(30, "content_30");
+        Set<Integer> keys = map.keySet();
+        map.find(15);
+        map.find(15);
+        map.find(15);
+        map.find(0);
+        map.find(30);
+        map.add(40, "content_40");
+        assertEquals(3, map.size());
+        assertTrue(keys.contains(15));
+        assertTrue(keys.contains(30));
+        assertTrue(keys.contains(40));
+        assertFalse(keys.contains(0));
+    }
+
+    @Test
+    public void testForgettingMapWithMultipleAddsAndFind(){
+        ForgettingMap map = new ForgettingMap(3);
+        RandomString random = new RandomString();
+        for (int i = 0; i < 500; i++) {
+            map.add(i, random.nextString());
+        }
+        Set<Integer> keys = map.keySet();
+        assertFalse(keys.contains(0));
+    }
+
+    @Test
+    public void testForgettingMapWithAddConcurrency() throws InterruptedException {
+        int numberOfThreads = 25;
+        ForgettingMap map = new ForgettingMap(3);
+        RandomString random = new RandomString();
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < numberOfThreads; i++) {
+            service.execute(() -> {
+                latch.countDown();
+                map.add(random.nextString(), random.nextString());
+            });
+        }
+        latch.await();
+        assertEquals(3, map.keySet().size());
+    }
+
+    @Test
+    public void testForgettingMapWithAddAndFindConcurrency() throws InterruptedException {
+        int numberOfThreads = 25;
+        ForgettingMap map = new ForgettingMap(3);
+        RandomString random = new RandomString();
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        ConcurrentSkipListSet<String> addedKeys = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < numberOfThreads; i++) {
+            service.execute(() -> {
+                latch.countDown();
+                String key = random.nextString();
+                addedKeys.add(key);
+                map.add(key, random.nextString());
+                for (String et: addedKeys) {
+                    map.find(et);
+                }
+            });
+        }
+        latch.await();
+        System.out.println(map.keySet());
+        assertEquals(3, map.keySet().size());
     }
 
 }
